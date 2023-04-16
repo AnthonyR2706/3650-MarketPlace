305df818e6631bfb649bb17d3acfc4acc3745a84
AnthonyR2706 <70709893+AnthonyR2706@users.noreply.github.com>
2023-04-16 15:31:19 -0400

    Update newjs.js

--- js/newjs.js
+++ js/newjs.js
@@ -1,1 +1,34 @@
 
+var allCheckboxes = document.querySelectorAll('input[type=checkbox]');
+var allItems = Array.from(document.querySelectorAll('.item'));
+var checked = {};
+
+getChecked('textbooks');
+getChecked('electronics');
+
+Array.prototype.forEach.call(allCheckboxes, function (el) {
+  el.addEventListener('change', toggleCheckbox);
+});
+
+function toggleCheckbox(e) {
+  getChecked(e.target.name);
+  setVisibility();
+}
+
+function getChecked(name) {
+  checked[name] = Array.from(document.querySelectorAll('input[name=' + name + ']:checked')).map(function (el) {
+    return el.value;
+  });
+}
+
+function setVisibility() {
+  allItems.map(function (el) {
+    var electronics = checked.electronics.length ? _.intersection(Array.from(el.classList), checked.electronics).length : true;
+    var textbooks = checked.textbooks.length ? _.intersection(Array.from(el.classList), checked.textbooks).length : true;
+    if (electronics && textbooks) {
+      el.style.display = '';
+    } else {
+      el.style.display = 'none';
+    }
+  });
+}
